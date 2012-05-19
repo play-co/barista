728d3e732c79947c87f950f66e1145b6c69f800f
Tom Fairfield <fairfieldt@gameclosure.com>
2012-05-19 16:55:49 -0700

    removing old index.js

--- engines/spidermonkey/index.js
+++
@@ -1,5 +0,0 @@
-exports.objectTemplate = require('./objectTemplate.js');
-exports.methodFormat = require('./methodFormat.js');
-exports.propertyFormat = require('./propertyFormat.js');
-exports.templateFormat = require('./templateFormat.js');
-
