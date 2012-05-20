f7b981dffb4f5e7ff491b5e4dbcc353d7fe14833
Tom Fairfield <fairfieldt@gameclosure.com>
2012-05-19 17:58:01 -0700

    added support for multiple files

--- barista.js
+++ barista.js
@@ -5,10 +5,18 @@ var argv = require('optimist')
 	.usage('Usage: $0 -e [engine] -f object.json')
 	.demand(['e', 'f'])
 	.alias('e', 'engine')
-	.alias('f', 'file')
 	.describe('e', 'the engine to generate object wrappers for')
+	.string('e')
+	.alias('f', 'file')
 	.describe('f', 'the file describing the object to be wrapped')
+	.string('f')
 	.argv;
 
 
-generator.run(argv.e, argv.f);
+var files = argv.f;
+if (!(files instanceof Array)) {
+	files = [files];
+}
+files.forEach(function(file) {
+	generator.run(argv.e, file);
+});
