db81574109d9178816dc3e53508b3637db451aad
Tom Fairfield <fairfieldt@gameclosure.com>
2012-05-19 21:22:33 -0700

    making engine.create use async.parallel and async fs.writeFile

--- engine.js
+++ engine.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var async = require('async');
 var templates = require('./templates').templates;
 
 var _load = function(engineName, templateName) {
@@ -6,7 +7,7 @@ var _load = function(engineName, templateName) {
 	var template = fs.readFileSync(templatePath).toString();
 	return template;
 };
-exports.load = function(engineName) {
+exports.load = function(engineName, cb) {
 	var engine = {};
 	templates.forEach(function(template) {
 		engine[template] = _load(engineName, template);
@@ -16,13 +17,29 @@ exports.load = function(engineName) {
 };
 
 
+var writeTemplate = function(path) {
+	return function(cb) {
+		console.log('writing', path);
+		fs.writeFile(path, '', cb);
+	};
+};
 exports.create = function(name) {
 	enginePath = __dirname + '/engines/' + name;
-	fs.mkdirSync(enginePath);
 	
-	templates.forEach(function(template) {
-		fs.writeFileSync(enginePath + '/' + template + '.bar', '');	
-	});
+	async.parallel(
+		[fs.mkdir.bind(fs, enginePath)].concat(
+		templates.map(function(template) {
+			return writeTemplate(enginePath + '/' + template + '.bar');
+		})),
+		function(err, result) {
+			if (err) {
+				console.log('Error building templates for engine', name);
+			} else {
+				console.log('Templates for engine', name, 'built!');
+			}
+		}
+	);
+
 };
 
 if (require.main === module) {
