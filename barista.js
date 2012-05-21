f23354222dab7ba6ff177a704f1e8295f87e32ff
Tom Fairfield <fairfieldt@gameclosure.com>
2012-05-21 11:54:50 -0700

    add support for create from commandline

--- barista.js
+++ barista.js
@@ -2,21 +2,28 @@ var generator = require('./generator');
 
 
 var argv = require('optimist')
-	.usage('Usage: $0 -e [engine] -f object.json')
-	.demand(['e', 'f'])
+	.usage('Usage: $0 -e [engine]  object.json ... | -c')
+	.demand(['e'])
 	.alias('e', 'engine')
 	.describe('e', 'the engine to generate object wrappers for')
 	.string('e')
-	.alias('f', 'file')
 	.describe('f', 'the file describing the object to be wrapped')
-	.string('f')
+	.alias('c', '--create')
 	.argv;
 
 
-var files = argv.f;
-if (!(files instanceof Array)) {
-	files = [files];
+if (argv.c) {
+	require('./engine').create(argv.e);
+} else {
+	var files = argv._;
+	if (!files.length) {
+		console.log('missing object description files');
+		process.exit(1);
+	}
+	if (!(files instanceof Array)) {
+		files = [files];
+	}
+	files.forEach(function(file) {
+		generator.run(argv.e, file);
+	});
 }
-files.forEach(function(file) {
-	generator.run(argv.e, file);
-});
